2c8ee5efb92a9e3ba6533352e9545eba6a9049dc
Caroline Caprini <caroline.caprini@hotmail.com>
2021-05-27 22:48:50 -0300

    Criação e teste de banco de dados.

--- public/livros.php
+++ public/livros.php
@@ -14,8 +14,8 @@
 <body>
     <div class="content">
         <h1>Bibliófilo's</h1>
-
         <h2>Livros</h2>
+
         <?php
         require 'mysql_server.php';
 
@@ -24,50 +24,54 @@
         $titulo = 'titulo';
         $autor = 'autor';
         $classificacao = 'classificacao';
+        $editora = 'editora';
+        $ano = 'ano';
+        $edicao = 'edicao';
+
         /*TODO-1: Adicione uma variavel para cada coluna */
+ 
+        $sql = "SELECT $titulo, $autor, $classificacao, $editora, $ano, $edicao FROM Livros";    
 
-
-        $sql =
-            'SELECT ' . $titulo .
-            '     , ' . $autor .
-            '     , ' . $classificacao .
-            /*TODO-2: Adicione cada variavel a consulta abaixo */
-            '  FROM livros';
-
+        /*TODO-2: Adicione cada variavel a consulta abaixo */
+             
 
         $resultado = mysqli_query($conexao, $sql);
         if (!$resultado) {
             echo mysqli_error($conexao);
         }
 
+        /* TODO-3: Adicione as variaveis ao cabeçalho da tabela */
 
-
-        $cabecalho =
-            '<table>' .
-            '    <tr>' .
-            '        <th>' . $titulo . '</th>' .
-            '        <th>' . $autor . '</th>' .
-            /* TODO-3: Adicione as variaveis ao cabeçalho da tabela */
-            '        <th>' . $classificacao . '</th>' .
-            '    </tr>';
+        $cabecalho ="
+            <table>
+                <tr> 
+                    <th> $titulo </th>
+                    <th> $autor </th>
+                    <th> $classificacao </th>
+                    <th> $editora </th>
+                    <th> $ano </th>
+                    <th> $edicao </th>
+                </tr>";
 
         echo $cabecalho;
+
+        /* TODO-4: Adicione a tabela os novos registros. */
 
         if (mysqli_num_rows($resultado) > 0) {
 
             while ($registro = mysqli_fetch_assoc($resultado)) {
-                echo '<tr>';
-
-                echo '<td>' . $registro[$titulo] . '</td>' .
-                    '<td>' . $registro[$autor] . '</td>' .
-                    /* TODO-4: Adicione a tabela os novos registros. */
-                    '<td>' . $registro[$classificacao] . '</td>';
-                echo '</tr>';
+                echo "
+                    <tr>
+                    <td> $registro[$titulo] </td>
+                    <td> $registro[$autor] </td>
+                    <td> $registro[$classificacao]</td>
+                    <td> $registro[$editora]</td>
+                    <td> $registro[$ano]</td>
+                    <td> $registro[$edicao]</td>
+                    </tr>";
             }
-            echo '</table>';
-        } else {
-            echo '';
-        }
+        } 
+        echo '</table>';
         FecharConexao($conexao);
         ?>
     </div>
